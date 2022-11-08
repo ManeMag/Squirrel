996ffdaa48b4ca439063edcf96d35cf8852999e3
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-11-08 19:49:39 +0200

    Added unit tests

--- Back-end/Squirrel/Controllers/StatisticsController.cs
+++ Back-end/Squirrel/Controllers/StatisticsController.cs
@@ -6,6 +6,8 @@ using Squirrel.Services.Abstractions;
 
 namespace Squirrel.Controllers
 {
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public sealed class StatisticsController : AuthorizedControllerBase
     {
         private readonly IStatisticService _statisticService;
@@ -17,6 +19,11 @@ namespace Squirrel.Controllers
             _statisticService = statisticService;
         }
 
+        /// <summary>
+        /// Gets statistics for a specific period of time inclusive
+        /// </summary>
+        /// <param name="startDate">Start of the date time period to get statistics for</param>
+        /// <param name="endDate">End of the date time period to get statistics for</param>
         [HttpGet]
         public async Task<ActionResult<StatisticsViewModel>> GetStatistics(
             [FromQuery] DateTime startDate,
@@ -32,6 +39,9 @@ namespace Squirrel.Controllers
             return Ok(statisticResult.Value);
         }
 
+        /// <summary>
+        /// Gets statistics for the last month - 30 days
+        /// </summary>
         [HttpGet("month")]
         public async Task<ActionResult<StatisticsViewModel>> GetStatisticsForMonth()
         {
@@ -45,6 +55,9 @@ namespace Squirrel.Controllers
             return Ok(statisticResult.Value);
         }
 
+        /// <summary>
+        /// Gets statistics for the last half year - 180 days
+        /// </summary>
         [HttpGet("half-year")]
         public async Task<ActionResult<StatisticsViewModel>> GetStatisticsForHalfYear()
         {
