2553c73a43417c56f746980fdee02eefb534d1bb
Alciel-Geneirodan <yevhenii.krupchak@nure.ua>
2022-11-16 02:31:43 +0300

    Replaced Volley with Ktor Client

--- Mobile/app/src/main/java/com/example/squirrel/AuthorizationActivity.kt
+++ Mobile/app/src/main/java/com/example/squirrel/AuthorizationActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.toolbox.Volley
+import androidx.lifecycle.lifecycleScope
+import com.example.squirrel.Program.Companion.client
+import com.example.squirrel.Program.Companion.domain
+import com.example.squirrel.Program.Companion.port
+import com.example.squirrel.Program.Companion.protocol
 import com.example.squirrel.databinding.ActivityAuthorizationBinding
-import com.example.squirrel.http.StringRequest
-import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.UnsupportedEncodingException
-
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.launch
 
 class AuthorizationActivity : AppCompatActivity()  {
 
@@ -23,17 +26,16 @@ class AuthorizationActivity : AppCompatActivity()  {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         val intent = Intent(this, MainActivity::class.java)
-        val stringRequest = StringRequest(
-            Request.Method.GET,
-            "http://geneirodan.zapto.org:23451/api/account/email",
-            { startActivity(intent) },
-            {
+
+        lifecycleScope.launch{
+            val response: HttpResponse = client.get("$protocol://$domain:$port/api/account/name")
+            if(response.status == HttpStatusCode.OK)
+                startActivity(intent)
+            else {
                 binding = ActivityAuthorizationBinding.inflate(layoutInflater)
                 setContentView(binding.root)
-            },
-            ""
-        )
-        Program.getRequestQueue()!!.add(stringRequest)
+            }
+        }
     }
 
     fun onClickGoRegister (view: View) = startActivity(Intent(this, RegisterActivity::class.java))
@@ -45,61 +47,23 @@ class AuthorizationActivity : AppCompatActivity()  {
             return
         }
         val context = this
-        val jsonBody = JSONObject()
-        jsonBody.put("email", binding.loginPrompt.text.toString())
-        jsonBody.put("password", binding.passwordPrompt.text.toString())
-        val requestBody = jsonBody.toString()
-        val stringRequest = StringRequest(
-            Request.Method.POST,
-            "http://geneirodan.zapto.org:23451/api/account/authenticate?callbackurl=geneirodan.zapto.org",
-            {
+        lifecycleScope.launch{
+            val response: HttpResponse = client.submitForm(
+                url = "$protocol://$domain:$port/api/account/login",
+                formParameters = Parameters.build {
+                    append("email", binding.loginPrompt.text.toString())
+                    append("password", binding.passwordPrompt.text.toString())
+                }
+            )
+            if(response.status == HttpStatusCode.OK) {
                 Toast.makeText(context, "Welcome!", Toast.LENGTH_LONG).show()
                 startActivity(intent)
-            },
-            { error ->
-                val response = error.networkResponse
-                if (response != null) {
-                    try {
-                        val res = String(response.data, Charsets.UTF_8)
-                        val arr = JSONArray(res).toList()
-                        for(x in arr)
-                            Toast.makeText(context, x.toString(), Toast.LENGTH_SHORT).show()
-                    } catch (e1 : UnsupportedEncodingException) {
-                        e1.printStackTrace()
-                    } catch (e2 : JSONException) {
-                        e2.printStackTrace()
-                    }
-                }
-            }, requestBody)
-        Program.getRequestQueue()!!.add(stringRequest)
-    }
-    @Throws(JSONException::class)
-    fun JSONObject.toMap(): Map<String, Any> {
-        val map = mutableMapOf<String, Any>()
-        val keysItr: Iterator<String> = this.keys()
-        while (keysItr.hasNext()) {
-            val key = keysItr.next()
-            var value: Any = this.get(key)
-            when (value) {
-                is JSONArray -> value = value.toList()
-                is JSONObject -> value = value.toMap()
             }
-            map[key] = value
-        }
-        return map
-    }
-
-    @Throws(JSONException::class)
-    fun JSONArray.toList(): List<Any> {
-        val list = mutableListOf<Any>()
-        for (i in 0 until this.length()) {
-            var value: Any = this[i]
-            when (value) {
-                is JSONArray -> value = value.toList()
-                is JSONObject -> value = value.toMap()
+            else {
+                val arr = response.body<List<String>>()
+                for(x in arr)
+                    Toast.makeText(context, x, Toast.LENGTH_SHORT).show()
             }
-            list.add(value)
         }
-        return list
     }
 }
