e226fa5d7a63aa3ee8f8419a2d5ca55ecebe93dd
Alciel-Geneirodan <yevhenii.krupchak@nure.ua>
2022-12-22 21:27:26 +0300

    GET test

---
+++ Mobile/app/src/main/java/com/example/squirrel/models/Statistics.kt
@@ -0,0 +1,21 @@
+package com.example.squirrel.models
+
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Impact(
+    var categoryId: Int,
+    var totalPercentage: Double,
+    var totalMoney: Double,
+    var transactionsCount: Int
+)
+@Serializable
+class Statistics (
+    var impact : List<Impact>,
+    var startDate : LocalDateTime,
+    var endDate : LocalDateTime,
+    var receivedAt: LocalDateTime,
+    var income : Double,
+    var outcome : Double
+    )
